b1ebe97be95ffc6d130ddd95f62ba69ca73b4e47
gwengusc <32805596+gwengusc@users.noreply.github.com>
2017-10-22 16:46:49 -0700

    Add files via upload

---
+++ C4_detect_test.cpp
@@ -0,0 +1,78 @@
+
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/ml/ml.hpp>
+#include <C4Feature.hpp>
+#include <C4Detector.hpp>
+
+
+int main() {
+
+//    cv::Mat src=cv::imread("/Users/wengguifan/Pictures/0428_nws_ocr-l-fatal-035.jpg");
+        cv::Mat src=cv::imread("/Users/wengguifan/pedestrian_data_test/pedestrian_image/INRIA/crop_000001.png");
+
+        C4Feature feature;
+
+        //C4Detector(C4Feature _feature, double _thresh = 0.8) {
+        C4Detector detector(feature);
+
+        std::vector<cv::Rect>detectedRects;
+        std::vector<cv::Rect>results;
+
+        struct timeval tpstart, tpend;
+        double timeuse;
+
+        gettimeofday(&tpstart, NULL);
+
+        detector.MultiDetecte(src,2,0.8,detectedRects);
+
+        gettimeofday(&tpend, NULL);
+
+
+        timeuse = 1000000 * (tpend.tv_sec - tpstart.tv_sec) + tpend.tv_usec - tpstart.tv_usec;
+
+        std::cout << "every image:" << timeuse / 1000 << "ms" << std::endl;
+
+        std::cout << "detectedRects num:" << detectedRects.size() << std::endl;
+
+        detector.post_process_NMS(detectedRects,2, 0.7);
+
+        // cout<<"targets_size:"<<targets.size()<<endl;
+
+        detector.post_process_NMS(detectedRects,0, 0.7);
+
+        detector.post_process(detectedRects,results,src.rows,src.cols);
+
+
+
+
+        int array[6][3];
+
+        array[0][0]=255;
+        array[0][1]=0;
+        array[0][2]=0;
+
+        array[1][0]=0;
+        array[1][1]=255;
+        array[1][2]=0;
+
+        array[2][0]=0;
+        array[2][1]=0;
+        array[2][2]=255;
+
+        int k=0;
+        for(int i = 0; i < results.size(); i++)
+        {
+                k=i%3;
+                cv::rectangle(src, results[i],cv::Scalar(array[k][0],array[k][1],array[k][2]),2 );
+        }
+
+        cv::imshow("result",src);
+        cv::waitKey( 0 );
+
+        return 0;
+}
